dedabd6470452f27dc32c318511f1f6a3e271214
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-04-17 21:46:24 -0400

    Append node to graph

--- js/ForceGraph.js
+++ js/ForceGraph.js
@@ -7,10 +7,49 @@ d3.json(dataUrl, function(json) {
 	var height = 600;
 	var width = 600;
 
+	var dataLinks = dataSet.links;
+	var dataNodes = dataSet.nodes;
+
 	d3.select(".forcegraph-title").text("Force-directed Graph of National Contiguity");
 
 	var svg = d3.select(".forcegraph")
 		.attr("width", width)
 		.attr("height", height)
-		.attr("viewBox", "0 0 550 550");
+		.attr("viewBox", "-200 -200 550 550");
+
+	var simulation = d3.forceSimulation()
+		.force("link", d3.forceLink().distance(20).strength(1).id(function(d) {
+			return d.country;
+		}))
+		.force("charge", d3.forceManyBody())
+		.force("center", d3.forceCenter( width/2, height/2 ));
+
+	// Append Links
+	var link = svg.selectAll(".link")
+		.attr("class", "link")
+		.data(dataSet.links)
+		.enter()
+		.append("line");
+
+	// Append Nodes
+	var node = svg.selectAll(".node")
+		.attr("class", "node")
+		.data(dataSet.nodes)
+		.enter()
+		.append("circle")
+			.attr("r", 5)
+			.attr("fill", "#fff");
+
+	simulation
+		.nodes(dataSet.nodes);
+
+	function ticked(e) {
+		/* link
+			.attr("x1", function(d) { return d.source.x; })
+			.attr("y1", function(d) { return d.source.y; })
+			.attr("x2", function(d) { return d.target.x; })
+			.attr("y1", function(d) { return d.target.x; })
+		*/
+	}
+
 });
